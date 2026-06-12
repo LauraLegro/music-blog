f044bf969258b9c1296017b8dfeefaa53aa29465
Laura Legro <allaura81@gmail.com>
2026-06-11 20:01:47 -0700

    fix: remove fileURLToPath, simplify api.js

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -1,17 +1,11 @@
 import express from "express";
 import serverless from "serverless-http";
 import postsRouter from "../../routes/posts.js";
-import { fileURLToPath } from "url";
-import path from "path";
 
-const _filename = fileURLToPath(import.meta.url);
-const _dirname = path.dirname(_filename);
 const app = express();
 
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(_dirname, "../../public")));
 app.set("view engine", "ejs");
-app.set("views", path.join(_dirname, "../../views"));
 
 app.use("/posts", postsRouter);
 
