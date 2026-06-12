d80b6f4df0826989ea0e25efd07732c0cfea4393
Laura Legro <allaura81@gmail.com>
2026-06-11 19:27:50 -0700

    fix: correct api.js, fix ejs version, add absolute paths

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 import express from "express";
 import postsRouter from "./routes/posts.js";
+import { fileURLToPath } from "url";
+import path from "path";
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 const PORT = 3000;
