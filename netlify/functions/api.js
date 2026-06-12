8036ce7c5385b5c9ace1843a61cce6064aa6e4ac
Laura Legro <allaura81@gmail.com>
2026-06-11 20:06:45 -0700

    fix: use path.resolve for views and public

--- netlify/functions/api.js
+++ netlify/functions/api.js
@@ -1,11 +1,14 @@
 import express from "express";
 import serverless from "serverless-http";
 import postsRouter from "../../routes/posts.js";
+import path from "path";
 
 const app = express();
 
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.resolve("public")));
 app.set("view engine", "ejs");
+app.set("views", path.resolve("views"));
 
 app.use("/posts", postsRouter);
 
